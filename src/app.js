7285f0717859d8082b186ae66bc1f56ba883c541
Caleb <87406555+caleblopez96@users.noreply.github.com>
2023-05-05 18:32:13 -0700

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ let users = {};
 
 
 client.on('message', (channel, tags, message, self) => {
-	if(self) return;
+if(self) return;
     if(username === client.channels) {
         if(message === 'insert message here') {
             // do something based off message streamer wants to listen for
